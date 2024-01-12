b489950e3153f362a081ad0435d42a82f6711598
Enrique Algar Ceular <ealgar-c@student.42malaga.com>
2024-01-12 19:04:26 +0100

    feat: finished pixles

--- src/pixels.c
+++ src/pixels.c
@@ -6,11 +6,38 @@
 /*   By: ealgar-c <ealgar-c@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 17:46:05 by ealgar-c          #+#    #+#             */
-/*   Updated: 2024/01/12 17:55:52 by ealgar-c         ###   ########.fr       */
+/*   Updated: 2024/01/12 18:47:16 by ealgar-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/miniRT.h"
+
+t_inter	*get_closest_collision(double x, double y, t_info *info)
+{
+	t_inter	*tmp_inter;
+	t_inter	*new_inter;
+	t_shape	*tmp_shape;
+
+	tmp_shape = info->shapes_list;
+	tmp_inter = NULL;
+	while (tmp_shape)
+	{
+		if (tmp_shape->type == CY)
+			new_inter = inter_cy(info, x, y);
+		else if (tmp_shape->type == PL)
+			new_inter = inter_pl(info, x, y);
+		else if (tmp_shape->type == SP)
+			new_inter = inter_sp(info, x, y);
+		if (new_inter)
+		{
+			if (!tmp_inter)
+				tmp_inter = new_inter;
+			else if (new_inter->d < tmp_inter->d)
+				tmp_inter = new_inter;
+		}
+		tmp_shape = tmp_shape->next;
+	}
+}
 
 void	put_pixels(t_info *info)
 {
