64ff692475e5bf5b085cb2e1f427919e8763fc15
palucena <palucena@student.42malaga.com>
2024-01-17 14:32:55 +0100

    SP->center is wrong

--- src/sphere.c
+++ src/sphere.c
@@ -6,7 +6,7 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 18:13:17 by palucena          #+#    #+#             */
-/*   Updated: 2024/01/17 12:44:24 by palucena         ###   ########.fr       */
+/*   Updated: 2024/01/17 14:26:36 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,33 +79,36 @@ t_inter	*inter_sp(t_info *in, t_shape *sp, double i, double j)
 	p.x = in->pl->l + i;
 	p.y = in->pl->t - j;
 	p.z = (WIDTH / 2) / sin((in->cset->fov / 2) * (M_PI / 180)) + in->cset->point.z;
-//	if (in->cset->n_vec.k < 0)
-//		p.z = ((WIDTH / 2) / sin((in->cset->fov / 2) * (M_PI / 180))) * (-1) + in->cset->point.z;
+/* 	if (in->cset->n_vec.k < 0)
+		p.z = ((WIDTH / 2) / sin((in->cset->fov / 2) * (M_PI / 180))) * (-1) + in->cset->point.z; */
 	d = v_norm(v_get_from2(in->cset->point, p));
 	if (i == WIDTH / 2 && j == HEIGHT / 2)
 	//	printf("Coordenadas del pixel central: %f %f %f\n", p.x, p.y, p.z);
 	cc = v_get_from2(in->cset->point, sp->prop.c);
+	printf("%f   con %f %f %f   en %f %f %f\n", sp->prop.c.x, sp->prop.c.y, sp->prop.c.z, cc.k, p.x, p.y, p.z);
 	abc.x = pow(v_mod(d), 2);
-//	abc.y = v_mod(v_mult(v_mult(d, v_get_from2(sp->prop.c, p)), v2));
 	abc.y = 2 * (d.i * cc.i + d.j * cc.j + d.k * cc.k);
 	abc.z = pow(v_mod(v_get_from2(sp->prop.c, in->cset->point)), 2) - pow(sp->prop.rad, 2);
 	inter->d = quadratic_equation(abc.x, abc.y, abc.z);
-	if (i == WIDTH / 2 && j == HEIGHT / 2)
 	//	printf("Discriminante: %f\n", abc.y * abc.y - 4 * abc.x * abc.z);
-	if (i == WIDTH / 2 && j == HEIGHT / 2)
+	if (i == WIDTH / 2 && j == HEIGHT / 2 - 10)
 	{
 		printf("Camara: %f  %f  %f\n", in->cset->point.x, in->cset->point.y, in->cset->point.z);
 		printf("Pixel: %f  %f  %f\n", p.x, p.y, p.z);
 		printf("Dirección: %f  %f  %f\n", d.i, d.j, d.k);
+		printf("Vector CC: %f  %f  %f\n", cc.i, cc.j, cc.k);
+		printf("ABC: %f  %f  %f\n", abc.x, abc.y, abc.z);
 		printf("Distancia a colision en el centro del plano: %f\n", inter->d);
-		printf("ABC: %f  %f  %f    Distancia: %f\n\n", abc.x, abc.y, abc.z, inter->d);
+	//	printf("Centro de la esfera: %f %f %f\n", inter->q.x, inter->q.y, inter->q.z);
 	}
 	if (inter->d < 0)
 	{
+/* 		if (i == WIDTH / 2 && j == (HEIGHT / 2) - 5)
+			printf("P: %f  %f  %f ABC: %f  %f  %f  Distancia: %f\n\n", p.x, p.y, p.z, abc.x, abc.y, abc.z, inter->d); */
 		free (inter);
 		return (NULL);
 	}
-	inter->d *= sqrt(pow((p.x - sp->prop.c.x), 2) + pow((p.y - sp->prop.c.y), 2) + pow((p.z - sp->prop.c.z), 2));
+	//inter->d *= sqrt(pow((p.x - sp->prop.c.x), 2) + pow((p.y - sp->prop.c.y), 2) + pow((p.z - sp->prop.c.z), 2));
 	inter->q.x = in->cset->point.x + inter->d * in->cset->n_vec.i;
 	inter->q.y = in->cset->point.y + inter->d * in->cset->n_vec.j;
 	inter->q.z = in->cset->point.z + inter->d * in->cset->n_vec.k;
