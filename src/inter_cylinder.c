e0edd5d29d1aa64fd2937f93d2a236ed68d2b04a
Enrique Algar Ceular <ealgar-c@student.42malaga.com>
2024-01-22 15:59:05 +0100

    feat: fixing

--- src/inter_cylinder.c
+++ src/inter_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: ealgar-c <ealgar-c@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 18:14:03 by palucena          #+#    #+#             */
-/*   Updated: 2024/01/22 15:45:07 by ealgar-c         ###   ########.fr       */
+/*   Updated: 2024/01/22 15:58:33 by ealgar-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ static t_inter	*cy_topcover_inter(t_info *info, t_shape *cy, t_pixel px)
 	cover_c->index = 0;
 	cover_c->prop.n_vec = cy->prop.n_vec;
 	plane_inter = inter_pl(info, cover_c, px);
-	if (v_mod(v_get_from2(info->cset->point, plane_inter->q)) < cy->prop.rad)
+	if (plane_inter && v_mod(v_get_from2(info->cset->point, plane_inter->q)) < cy->prop.rad)
 	{
 		free (cover_c);
 		return (plane_inter);
@@ -52,7 +52,7 @@ static t_inter	*cy_botcover_inter(t_info *info, t_shape *cy, t_pixel px)
 	cover_c->index = 0;
 	cover_c->prop.n_vec = v_esc_mult(cy->prop.n_vec, -1);
 	plane_inter = inter_pl(info, cover_c, px);
-	if (v_mod(v_get_from2(info->cset->point, plane_inter->q)) < cy->prop.rad)
+	if (plane_inter && v_mod(v_get_from2(info->cset->point, plane_inter->q)) < cy->prop.rad)
 	{
 		free (cover_c);
 		return (plane_inter);
