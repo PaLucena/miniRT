6f04a2aca15b4412f47761bb995653aed9f93d8a
Enrique Algar Ceular <ealgar-c@student.42malaga.com>
2024-01-22 15:52:10 +0100

    feat: started cy

--- src/inter_cylinder.c
+++ src/inter_cylinder.c
@@ -6,13 +6,13 @@
 /*   By: ealgar-c <ealgar-c@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 18:14:03 by palucena          #+#    #+#             */
-/*   Updated: 2024/01/22 15:11:05 by ealgar-c         ###   ########.fr       */
+/*   Updated: 2024/01/22 15:45:07 by ealgar-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-t_inter	*cy_topcover_inter(t_info *info, t_shape *cy, t_pixel px)
+static t_inter	*cy_topcover_inter(t_info *info, t_shape *cy, t_pixel px)
 {
 	t_shape	*cover_c;
 	t_inter	*plane_inter;
@@ -26,11 +26,36 @@ t_inter	*cy_topcover_inter(t_info *info, t_shape *cy, t_pixel px)
 				* cy->prop.height / 2));
 	cover_c->index = 0;
 	cover_c->prop.n_vec = cy->prop.n_vec;
-	plane_inter	= inter_pl(info, cover_c, px);
+	plane_inter = inter_pl(info, cover_c, px);
 	if (v_mod(v_get_from2(info->cset->point, plane_inter->q)) < cy->prop.rad)
 	{
-		free (plane_inter);
-		return (cover_c);
+		free (cover_c);
+		return (plane_inter);
+	}
+	free (plane_inter);
+	free (cover_c);
+	return (NULL);
+}
+
+static t_inter	*cy_botcover_inter(t_info *info, t_shape *cy, t_pixel px)
+{
+	t_shape	*cover_c;
+	t_inter	*plane_inter;
+
+	cover_c = malloc(sizeof(t_shape));
+	cover_c->prop.c.x = (cy->prop.c.x - (cy->prop.n_vec.i
+				* cy->prop.height / 2));
+	cover_c->prop.c.y = (cy->prop.c.y - (cy->prop.n_vec.j
+				* cy->prop.height / 2));
+	cover_c->prop.c.z = (cy->prop.c.z - (cy->prop.n_vec.k
+				* cy->prop.height / 2));
+	cover_c->index = 0;
+	cover_c->prop.n_vec = v_esc_mult(cy->prop.n_vec, -1);
+	plane_inter = inter_pl(info, cover_c, px);
+	if (v_mod(v_get_from2(info->cset->point, plane_inter->q)) < cy->prop.rad)
+	{
+		free (cover_c);
+		return (plane_inter);
 	}
 	free (plane_inter);
 	free (cover_c);
@@ -39,23 +64,20 @@ t_inter	*cy_topcover_inter(t_info *info, t_shape *cy, t_pixel px)
 
 t_inter	*inter_cy(t_info *in, t_shape *cy, t_pixel px)
 {
-	t_inter		*inter_body;
+	// t_inter		*inter_body;
 	t_inter		*inter_top;
 	t_inter		*inter_bot;
 
-	inter_body = cy_body_inter();
-	inter_body = cy_topcover_inter();
-	inter_body = cy_botcover_inter();
-	inter->index = cy->index;
-	cc = v_get_from2(in->cset->point, sp->prop.c);
-	abc = calculate_abc(in, sp, px.d, cc);
-	inter->d = quadratic_equation(abc.x, abc.y, abc.z);
-	if (inter->d < 0)
-	{
-		free (inter);
-		return (NULL);
-	}
-	cc = v_norm(cc);
-	inter->q = inter_point_coords(in, inter, cc);
-	return (inter);
+	// inter_body = cy_body_inter();
+	inter_top = cy_topcover_inter(in, cy, px);
+	inter_bot = cy_botcover_inter(in, cy, px);
+/* 	if (!inter_top && !inter_bot && inter_body)
+		return (inter_body); */
+	if (inter_top && !inter_bot)
+		return (inter_top);
+	if (!inter_top && inter_bot)
+		return (inter_bot);
+	if (inter_top->d < inter_bot->d)
+		return (inter_top);
+	return (inter_bot);
 }
