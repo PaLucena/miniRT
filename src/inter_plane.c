84bedf3b8b8c414008423b7d03a287186acb6d9a
palucena <palucena@student.42malaga.com>
2024-02-04 20:29:10 +0100

    Plane working

--- src/inter_plane.c
+++ src/inter_plane.c
@@ -6,7 +6,7 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 18:12:56 by palucena          #+#    #+#             */
-/*   Updated: 2024/01/27 14:52:07 by palucena         ###   ########.fr       */
+/*   Updated: 2024/02/04 20:26:31 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,13 @@ double	distance_pl(t_info *info, t_shape *pl, t_vector ray)
 {
 	double	top;
 	double	bot;
-	double	d;
 
+	bot = v_dot_product(ray, pl->prop.n_vec);
+	if (bot == 0)
+		return (0);
 	top = v_dot_product(v_get_from2(info->lset->point, pl->prop.c),
 			pl->prop.n_vec);
-	bot = v_dot_product(ray, pl->prop.n_vec);
-	if (bot < 0.000001)
-		return (0);
-	d = (top / bot);
-	return (d);
+	return (top / bot);
 }
 
 t_inter	*inter_pl(t_info *in, t_shape *pl, t_pixel px)
@@ -37,7 +35,7 @@ t_inter	*inter_pl(t_info *in, t_shape *pl, t_pixel px)
 	if (!inter->d)
 		return (free(inter), NULL);
 	inter->q = inter_point_coords(in, inter, px.d, pl->type);
-	inter->q.x *= -1; //TODO: arreglar signos
-	inter->d *= (-1); //TODO: arreglar signos
+	if (test)
+		printf("%f\n", inter->d);
 	return (inter);
 }
