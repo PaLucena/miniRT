adec593895cd8ca034c863ac0f1240ddb9726055
Pablo Lucena González <pablolucena01@gmail.com>
2024-03-07 10:37:56 +0100

    fix: :zap: resize

--- bonus/src/utils/init_bonus.c
+++ bonus/src/utils/init_bonus.c
@@ -6,7 +6,7 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 14:17:59 by ealgar-c          #+#    #+#             */
-/*   Updated: 2024/03/05 18:27:25 by palucena         ###   ########.fr       */
+/*   Updated: 2024/03/07 10:37:43 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	init_mlx(t_info	*info)
 {
-	info->mlx_s.mlx = mlx_init(WIDTH, HEIGHT, "miniRT", false);
+	info->mlx_s.mlx = mlx_init(WIDTH, HEIGHT, "miniRT", true);
 	info->mlx_s.win = mlx_new_image(info->mlx_s.mlx, WIDTH, HEIGHT);
 	mlx_close_hook(info->mlx_s.mlx, &ft_exit_program, (void *)info);
 	mlx_loop_hook(info->mlx_s.mlx, &ft_keyhook, (void *)info);
